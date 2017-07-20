2c545c57177fd713e15aee9e3217525465b3add9
taoxf <taoxiaofeng55@qq.com>
2017-07-20 20:55:55 +0800

    this 指向，谁调用this，this就指向谁

---
+++ this.js
@@ -0,0 +1,24 @@
+//this 谁调用指向谁
+
+// this.m = 1000;
+// function test() {
+// 	console.log(this.m)
+// }
+// test();
+this.m = 1000;
+var obj = {
+	m : 100,
+	test:function(){
+		console.log(this.m);
+		return function () { //闭包   这个function指向了外面的window
+			console.log(this.m);
+		}
+	}
+};
+(obj.test())();
+ //相当于
+ var t = obj.test();
+ t();
+ //等于
+ window.t();
+//里面这个function指向了外面的window
