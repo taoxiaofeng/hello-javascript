f255e8347a134edc0e1138415c761e1643e6fe64
taoxf <taoxiaofeng55@qq.com>
2017-07-21 16:14:45 +0800

    截取字符串demo

--- this.js
+++ this.js
@@ -5,20 +5,26 @@
 // 	console.log(this.m)
 // }
 // test();
-this.m = 1000;
-var obj = {
-	m : 100,
-	test:function(){
-		console.log(this.m);
-		return function () { //闭包   这个function指向了外面的window
-			console.log(this.m);
-		}
-	}
-};
-(obj.test())();
- //相当于
- var t = obj.test();
- t();
- //等于
- window.t();
-//里面这个function指向了外面的window
+// this.m = 1000;
+// var obj = {
+// 	m : 100,
+// 	test:function(){
+// 		console.log(this.m);
+// 		return function () { //闭包   这个function指向了外面的window
+// 			console.log(this.m);
+// 		}
+// 	}
+// };
+// (obj.test())();
+//  //相当于
+//  var t = obj.test();
+//  t();
+//  //等于
+//  window.t();
+// //里面这个function指向了外面的window
+// 
+function test() {
+	console.log(this.style.color);
+}
+
+document.getElementById('test').onclick = test;
