ecee90b2edfaec235104bed3617b0ee9f662e162
taoxiaofeng <taoxiaofeng55@qq.com>
2019-03-24 16:04:08 +0800

    Object

--- dataType/data-type.js
+++ dataType/data-type.js
@@ -174,14 +174,64 @@
  * 2、释放内存
  *      局部变量:函数执行完自动释放
  *      对象： 成为垃圾对象 ==> 垃圾回收器回收
- */     
+ */
 
- var a = 3;
- var obj = {};
- obj = undefined;
+// var a = 3;
+// var obj = {};
+// obj = undefined;
 
- function fn() {
-     var b = {};
- }
+// function fn() {
+//     var b = {};
+// }
 
- fn() //b是自动释放，b所指向的对象是在后面的某个时刻由垃圾回收器回收
+// fn() //b是自动释放，b所指向的对象是在后面的某个时刻由垃圾回收器回收
+
+/**
+ * 1、什么是对象？
+ *      多个数据的封装体
+ *      用来保存多个数据的容器
+ *      一个对象代表现实中的一个事物
+ * 2、为什么要用对象？
+ *      统一管理多个数据
+ * 3、对象的组成？
+ *      属性：属性名(字符串)和属性值（任意）组成
+ *      方法：一种特别的属性（属性值是函数）
+ * 4、如何访问对象内部数据？
+ *      属性名：编码简单 有时候不能用
+ *      ['属性名']：编码麻烦 可以通用
+ * 
+ */
+
+// var p = {
+//     name: 'Tom',
+//     age: 12,
+//     setName: function (name) {
+//         this.name = name;
+//     },
+//     setAge: function (age) {
+//         this.age = age;
+//     }
+// }
+
+// p.setName('Bob');
+// p['setAge'](23);
+// console.log(p.name, p['age'])
+
+/**
+ *  什么时候必须使用['属性名']的方式？
+ *      1、属性名包含特殊字符：- 空格
+ *      2、变量名不确定
+ */
+
+var p = {};
+//1、给p对象添加一个属性：content type: text/json
+// p.content.type = 'text/json'
+p['content-type'] = 'text/json';
+console.log(p['content-type']);
+
+//2、变量名不确定
+var propName = 'myAge';
+var value = 18;
+
+//p.propName = value //不可用
+p[propName] = value;
