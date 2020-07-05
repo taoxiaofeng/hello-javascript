859d14d6fcfeee24ac655cf185ae56035d5d29ee
taoxf <taoxf@ipharmacare.net>
2020-07-05 17:25:38 +0800

    cors jsonp

---
+++ 2020/202007/JSONP/serverJSONP.js
@@ -0,0 +1,16 @@
+let express = require('express');
+
+let app = express();
+
+app.listen(8001, _ => {
+    console.log('OK!');
+});
+
+app.get('/list', (req, res) =>{
+    let { callback = Function.prototype }  = req.query;
+    let data = {
+        code:0,
+        message: '请求成功'
+    };
+    res.send(`${callback}(${JSON.stringify(data)})`);
+})
