21bb02e2ac048048d94621271c4b0308e2e3ccf7
taoxiaofeng <taoxiaofeng@mchz.com.cn>
2022-04-12 00:15:23 +0800

    函数柯里化

---
+++ 20220411/curry.js
@@ -0,0 +1,32 @@
+// 函数柯里化
+// const foo = function(...args) {}
+
+// foo(1,2,3) == 6; // true
+// foo(1)(2,3) == 6; // true
+// foo(1)(2)(3)(4) == 10; //true
+
+function argsSum(args) {
+  return args.reduce((pre, cur) => {
+    return pre + cur;
+  }, 0)
+}
+
+const foo = (...args1) => {
+  const sum1 = argsSum(args1);
+  const fn = (...args2) => {
+    const sum2 = argsSum(args2);
+    return foo(sum1 + sum2);
+  };
+
+  // 核心内容
+  // 函数调用toString 方法转换为基本类型
+  fn.toString = () => {
+    return sum1;
+  }
+    // console.log(fn)
+  return fn;
+}
+
+console.log(foo(1,2,3) == 6); // true
+console.log(foo(1)(2,3) == 6); // true
+console.log(foo(1)(2)(3)(4) == 10); //true
