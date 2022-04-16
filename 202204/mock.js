1bc3205408d0128212d0f552e0dce0914c3529a8
taoxiaofeng <taoxiaofeng@mchz.com.cn>
2022-04-16 19:46:54 +0800

    lodash pick 方法测试

---
+++ 202204/mock.js
@@ -0,0 +1,860 @@
+const list = [
+  {
+    "targetValue": "30.0",
+    "max": "100.0",
+    "min": "90.0",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "99.0",
+    "max": "90.0",
+    "min": "80.0",
+    "level": "A",
+    "type": ""
+  },
+  {
+    "targetValue": "99.0",
+    "max": "100.0",
+    "min": "90.0",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "99.0",
+    "max": "100.0",
+    "min": "90.0",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "99.0",
+    "max": "100.0",
+    "min": "90.0",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "99.0",
+    "max": "100.0",
+    "min": "90.0",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "99.0",
+    "max": "100.0",
+    "min": "90.0",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "90.0",
+    "max": "100.0",
+    "min": "90.0",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "90.0",
+    "max": "100.0",
+    "min": "90.0",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "90.0",
+    "max": "100.0",
+    "min": "90.0",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "90.0",
+    "max": "100.0",
+    "min": "90.0",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "",
+    "max": "",
+    "min": "",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "",
+    "max": "",
+    "min": "",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "",
+    "max": "",
+    "min": "",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "",
+    "max": "",
+    "min": "",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "",
+    "max": "",
+    "min": "",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "99.0",
+    "max": "100.0",
+    "min": "90.0",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "99.0",
+    "max": "100.0",
+    "min": "90.0",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "99.0",
+    "max": "100.0",
+    "min": "90.0",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "99.0",
+    "max": "100.0",
+    "min": "90.0",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "99.0",
+    "max": "100.0",
+    "min": "90.0",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "90.0",
+    "max": "100.0",
+    "min": "90.0",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "90.0",
+    "max": "100.0",
+    "min": "90.0",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "90.0",
+    "max": "100.0",
+    "min": "90.0",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "90.0",
+    "max": "100.0",
+    "min": "90.0",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "30.0",
+    "max": "100.0",
+    "min": "90.0",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "",
+    "max": "",
+    "min": "",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "",
+    "max": "",
+    "min": "",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "",
+    "max": "",
+    "min": "",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "",
+    "max": "",
+    "min": "",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "",
+    "max": "",
+    "min": "",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "99.0",
+    "max": "90.0",
+    "min": "80.0",
+    "level": "A",
+    "type": ""
+  },
+  {
+    "targetValue": "99.0",
+    "max": "100.0",
+    "min": "90.0",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "99.0",
+    "max": "100.0",
+    "min": "90.0",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "99.0",
+    "max": "100.0",
+    "min": "90.0",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "99.0",
+    "max": "100.0",
+    "min": "90.0",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "99.0",
+    "max": "100.0",
+    "min": "90.0",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "90.0",
+    "max": "100.0",
+    "min": "90.0",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "90.0",
+    "max": "100.0",
+    "min": "90.0",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "90.0",
+    "max": "100.0",
+    "min": "90.0",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "90.0",
+    "max": "100.0",
+    "min": "90.0",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "",
+    "max": "",
+    "min": "",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "",
+    "max": "",
+    "min": "",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "",
+    "max": "",
+    "min": "",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "",
+    "max": "",
+    "min": "",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "",
+    "max": "",
+    "min": "",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "99.0",
+    "max": "100.0",
+    "min": "90.0",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "99.0",
+    "max": "100.0",
+    "min": "90.0",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "99.0",
+    "max": "100.0",
+    "min": "90.0",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "99.0",
+    "max": "100.0",
+    "min": "90.0",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "99.0",
+    "max": "100.0",
+    "min": "90.0",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "90.0",
+    "max": "100.0",
+    "min": "90.0",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "90.0",
+    "max": "100.0",
+    "min": "90.0",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "90.0",
+    "max": "100.0",
+    "min": "90.0",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "90.0",
+    "max": "100.0",
+    "min": "90.0",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "30.0",
+    "max": "100.0",
+    "min": "90.0",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "30.0",
+    "max": "100.0",
+    "min": "90.0",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "",
+    "max": "",
+    "min": "",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "",
+    "max": "",
+    "min": "",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "",
+    "max": "",
+    "min": "",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "",
+    "max": "",
+    "min": "",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "",
+    "max": "",
+    "min": "",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "99.0",
+    "max": "90.0",
+    "min": "80.0",
+    "level": "A",
+    "type": ""
+  },
+  {
+    "targetValue": "99.0",
+    "max": "90.0",
+    "min": "80.0",
+    "level": "A",
+    "type": ""
+  },
+  {
+    "targetValue": "99.0",
+    "max": "100.0",
+    "min": "90.0",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "99.0",
+    "max": "100.0",
+    "min": "90.0",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "99.0",
+    "max": "100.0",
+    "min": "90.0",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "99.0",
+    "max": "100.0",
+    "min": "90.0",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "99.0",
+    "max": "100.0",
+    "min": "90.0",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "99.0",
+    "max": "100.0",
+    "min": "90.0",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "99.0",
+    "max": "100.0",
+    "min": "90.0",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "99.0",
+    "max": "100.0",
+    "min": "90.0",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "99.0",
+    "max": "100.0",
+    "min": "90.0",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "99.0",
+    "max": "100.0",
+    "min": "90.0",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "90.0",
+    "max": "100.0",
+    "min": "90.0",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "90.0",
+    "max": "100.0",
+    "min": "90.0",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "90.0",
+    "max": "100.0",
+    "min": "90.0",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "90.0",
+    "max": "100.0",
+    "min": "90.0",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "90.0",
+    "max": "100.0",
+    "min": "90.0",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "90.0",
+    "max": "100.0",
+    "min": "90.0",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "90.0",
+    "max": "100.0",
+    "min": "90.0",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "90.0",
+    "max": "100.0",
+    "min": "90.0",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "",
+    "max": "",
+    "min": "",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "",
+    "max": "",
+    "min": "",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "",
+    "max": "",
+    "min": "",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "",
+    "max": "",
+    "min": "",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "",
+    "max": "",
+    "min": "",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "",
+    "max": "",
+    "min": "",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "",
+    "max": "",
+    "min": "",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "",
+    "max": "",
+    "min": "",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "",
+    "max": "",
+    "min": "",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "",
+    "max": "",
+    "min": "",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "99.0",
+    "max": "100.0",
+    "min": "90.0",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "99.0",
+    "max": "100.0",
+    "min": "90.0",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "99.0",
+    "max": "100.0",
+    "min": "90.0",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "99.0",
+    "max": "100.0",
+    "min": "90.0",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "99.0",
+    "max": "100.0",
+    "min": "90.0",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "90.0",
+    "max": "100.0",
+    "min": "90.0",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "90.0",
+    "max": "100.0",
+    "min": "90.0",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "90.0",
+    "max": "100.0",
+    "min": "90.0",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "90.0",
+    "max": "100.0",
+    "min": "90.0",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "30.0",
+    "max": "100.0",
+    "min": "90.0",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "",
+    "max": "",
+    "min": "",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "",
+    "max": "",
+    "min": "",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "",
+    "max": "",
+    "min": "",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "",
+    "max": "",
+    "min": "",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "",
+    "max": "",
+    "min": "",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "99.0",
+    "max": "90.0",
+    "min": "80.0",
+    "level": "A",
+    "type": ""
+  },
+  {
+    "targetValue": "99.0",
+    "max": "100.0",
+    "min": "90.0",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "99.0",
+    "max": "100.0",
+    "min": "90.0",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "99.0",
+    "max": "100.0",
+    "min": "90.0",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "99.0",
+    "max": "100.0",
+    "min": "90.0",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "99.0",
+    "max": "100.0",
+    "min": "90.0",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "90.0",
+    "max": "100.0",
+    "min": "90.0",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "90.0",
+    "max": "100.0",
+    "min": "90.0",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "90.0",
+    "max": "100.0",
+    "min": "90.0",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "90.0",
+    "max": "100.0",
+    "min": "90.0",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "",
+    "max": "",
+    "min": "",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "",
+    "max": "",
+    "min": "",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "",
+    "max": "",
+    "min": "",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "",
+    "max": "",
+    "min": "",
+    "level": "",
+    "type": ""
+  },
+  {
+    "targetValue": "",
+    "max": "",
+    "min": "",
+    "level": "",
+    "type": ""
+  }
+]
+
+export {
+  list
+};
