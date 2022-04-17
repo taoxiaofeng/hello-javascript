5e21d6913ad2b2bf6eaa0786a3e6a78d1ad73d6f
taoxiaofeng <taoxiaofeng@mchz.com.cn>
2022-04-17 20:05:48 +0800

    demo

--- 202204/lodashDemo.js
+++ 202204/lodashDemo.js
@@ -6,4 +6,13 @@ var getValues = () => {
 
 // console.log(getValues());
 // console.log(_.isEmpty({  }))
-console.log(Object.getOwnPropertyNames({ max: '', min: '', targetValue: '' }))
+// console.log(Object.getOwnPropertyNames({ max: '', min: '', targetValue: '' }))
+
+// 判断一个json对象中是否存在空的属性值
+let hasAnyValue = Object.values({ max: [], min: {}, targetValue: '', a: null, b: undefined, c: 'aaa' }).some(e =>
+  typeof e === 'string' ? !!e :
+    typeof e === 'number' ? !isNaN(e) :
+      Array.isArray(e) ? !!e.length :
+        e != null
+);
+console.log(hasAnyValue)
