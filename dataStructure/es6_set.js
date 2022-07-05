e46312d7d85be936134965747376dc947079403c
taoxiaofeng <taoxiaofeng55@qq.com>
2022-07-05 22:23:55 +0800

    es6

---
+++ dataStructure/es6_set.js
@@ -0,0 +1,23 @@
+// ES6 中的 Set();
+
+// var s = new Set();
+// s.add(1);
+// s.add(2);
+// s.add(3);
+// s.add(1);
+
+// s.forEach(function(value1, value2, set) {
+//   // {1:1,2:2,3:3}
+//   console.log('value 1 -- ', value1);
+//   console.log('value 2 -- ', value2);
+//   console.log('set -- ', set);
+// });
+
+// var arr = ['a', 'b', 'c'];
+// arr.forEach(function(item,index) {
+//   // item 数组存储的值， index 对应的下标
+//   console.log(item,index);
+// })
+
+var a = new Set([1,2,3]);
+var b = new Set([2,3,4]);
