c97b6e456591e5c5bfe9a6367b9c31c329a20ef8
taoxiaofeng <taoxiaofeng55@qq.com>
2023-04-13 00:12:50 +0800

    cors

--- 2023/CORS/server.js
+++ 2023/CORS/server.js
@@ -7,9 +7,17 @@ app.listen(90);
 
 // 91端口的服务
 var app2 = express();
+
+// 1、修改响应头解决跨域问题
+// app2.get("/", function(req, res) {
+//   // 增加响应头解决跨域问题
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.send("你好");
+// });
+
+// 2、使用jsonp解决跨域问题
 app2.get("/", function(req, res) {
-  // 增加响应头解决跨域问题
-  res.header('Access-Control-Allow-Origin', '*');
-  res.send("你好");
+  var funcname = req.query.callback;
+  res.send(funcname + "({name:'张三'})");
 });
 app2.listen(91);
