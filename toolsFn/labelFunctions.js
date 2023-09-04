b2b1438aec6950ed758cb18c71b48206f4b1d1ea
taoxiaofeng <taoxiaofeng55@qq.com>
2023-09-05 00:09:06 +0800

    标签函数

--- toolsFn/labelFunctions.js
+++ toolsFn/labelFunctions.js
@@ -45,20 +45,64 @@
 /**
  * 自定义标签函数 - 将模版字符串转换为大写形式
  */
-function upperCase(strings, ...values) {
-  let result = '';
-  for (let i = 0; i < strings.length; i++) {
-    result += strings[i];
-    if (i < values.length) {
-      result += values[i];
-    }
-  }
-  return result.toUpperCase();
-}
+// function upperCase(strings, ...values) {
+//   let result = '';
+//   for (let i = 0; i < strings.length; i++) {
+//     result += strings[i];
+//     if (i < values.length) {
+//       result += values[i];
+//     }
+//   }
+//   return result.toUpperCase();
+// }
 
-// 使用示例
-let x = "Hello";
-let y = "World";
+// // 使用示例
+// let x = "Hello";
+// let y = "World";
 
-const s1 = upperCase`1${x}2${y}3`;
-console.log(s1); // 1HELLO2WORLD3
+// const s1 = upperCase`1${x}2${y}3`;
+// console.log(s1); // 1HELLO2WORLD3
+
+
+/**
+ * 使用标签函数自动转移模版字符串中的特殊字符，防止XSS攻击
+ */
+// function safeHtml(strings, ...values) {
+//   let result = strings[0];
+//   for (let i = 0; i < strings.length; i++) {
+//     let val = String(values[i - 1]);
+//     result += val.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+//     result += strings[i];
+//   }
+//   return result;
+// }
+
+// function safeHtml(strings, ...values) {
+//   let result = strings[0];
+//   for (let i = 1; i < strings.length; i++) {
+//     let val = String(values[i - 1]);
+//     result += val.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+//     result += strings[i];
+//   }
+//   return result;
+// }
+
+// let userSuppliedInput = "<img src=x onerror=alert('XSS') />";
+// const result = safeHtml`<div>${userSuppliedInput}</div>`;
+// console.log(`first: ${result}`); // <div><img src=x onerror=alert('XSS')></div>
+
+/**
+ * 创建DSL
+ * 标签函数可以用于解析模版字符串中的特定语法， 从而创建DSL(领域特定语言)
+ */
+
+// 例如实现一个css标签函数
+const css = String.raw;
+
+
+
+const color = 'red';
+const fontSize = 20;
+const style = css`color: ${color}; font-size: ${fontSize}px;`;
+
+console.log(style)
